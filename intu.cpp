60bced9736b9d1935b84da794d441247a054b6c9
Snare drum <86771303+Saiderman@users.noreply.github.com>
2022-05-21 10:16:29 +1000

    Update intu.cpp

--- intu.cpp
+++ intu.cpp
@@ -5,7 +5,6 @@
 #include <vector>
 #include <queue>
 #include <map>
-#include <functional>
 using namespace std;
 
 class person {
@@ -17,6 +16,7 @@ class person {
 
 
 const string delimit = " ";
+// Exams sorted in terms of length of exam
 map<string, int> exam_type = {
 	{"HMX2", 0},
 	{"HM", 1},
@@ -30,7 +30,7 @@ map<string, int> exam_type = {
 	{"HMX1", 10}
 };
 
-
+// Custom operator to check if the lhs exam is 'less' than the rhs exam
 class comp{
 	public:
 		bool operator()(person lhs, person rhs) {
@@ -49,23 +49,26 @@ class comp{
 int main(){
 	priority_queue<person, vector<person>, comp> students;
 	int cols, stu_num, max_row = -1;
+	
+	// Scanning in layout of room
 	scanf("%d", &cols);
 	int rows[cols];
 	for (int i = 0; i<cols; i++) {
 		scanf("%d", &rows[i]);
 		if (rows[i] > max_row) max_row = rows[i];
 	}
+	
+	// Scanning in student info
 	scanf("%d", &stu_num);
-	//person students[stu_num];
 	for(int i = 0; i<stu_num; i++) {
 		person student;
 		scanf("%s", student.name.c_str());
 		scanf("%s", student.subject.c_str());
-		string s;
-		scanf("%s", s.c_str());
-		student.exam_num = stoi(s);
+		scanf("%d", student.exam_num);
 		students.push(student);
 	}
+	
+	// Seating students with shorter exams closer to door
 	person seating[max_row][cols];
 	for(int i = 0; i<max_row; i++) {
 		for(int j = cols-1; j>=0; j--) { //i and j value can be altered if corner of door changes
@@ -75,6 +78,8 @@ int main(){
 			}
 		}
 	}
+	
+	// Printing seating arrangement
 	for(int i = 0; i<max_row; i++) {
 		for(int j = 0; j<cols; j++) {
 			if (rows[j] > i) printf("%4s", seating[i][j].name.c_str());
@@ -83,8 +88,10 @@ int main(){
 		}
 		printf("\n");
 	}
-//	for(;!students.empty();students.pop()){
-//		printf("%s %s %d\n", students.top().name.c_str(), students.top().subject.c_str(), students.top().exam_num);
-//	}
+	
+	// Debugging for printing student details
+	//for(;!students.empty();students.pop()){
+	//	printf("%s %s %d\n", students.top().name.c_str(), students.top().subject.c_str(), students.top().exam_num);
+	//}
 	return 0;
 }
