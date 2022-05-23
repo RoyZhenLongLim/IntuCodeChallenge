eccf8359e231bb0003996d73b4604b6decef65cd
Roy Lim <roy.lim@student.unsw.edu.au>
2022-05-23 12:01:44 +1000

    Included more testing instructions

--- Python/intu.py
+++ Python/intu.py
@@ -27,17 +27,18 @@ class Student:
         self.exam_n = exam_n
 
 
-# Tmp variables
-# TODO: Finish snanning in the room
-NCOL = 6
-NROW = 15
+# Scan in the room, the format is {Number of rows in column A} {Number of rows in column B} ...
+# Returns number of columns and the number of rows in each column
+def create_layout():
+    layout = input().split()
+    # TODO: Include error checking
+    layout = [int(item) for item in layout]
+    return len(layout), layout
 
-if __name__ == '__main__':
 
-    # Scan in the room TODO
-
-    # Assuming the format is: {Student Number} {Subject} {Exam Number}
-    # Takes in all the students and stores their information
+# Assuming the format is: {Student Number} {Subject} {Exam Number}
+# Takes in all the students and stores their information
+def create_student_list():
     student_list = []
     student_info = input()
     while (student_info != ""):
@@ -51,12 +52,22 @@ if __name__ == '__main__':
         ))
         print(f"Student {student_info[0]} inserted sitting {student_info[1]} Exam {student_info[2]}")
         student_info = input()
+    return student_list
+
+if __name__ == '__main__':
+
+    # Figure out layout of the room
+    ncol, room_layout = create_layout()
+
+    # Create the list tof students
+    student_list = create_student_list()
 
     # Sort the list first by subject number and second by exam number (sorts first by subject, then by exam number)
     student_list.sort(key=lambda x: (x.subject_n, x.exam_n), reverse=True)
 
+    # Output the order students will be seated in
     for student in student_list:
-        print(
-            f"Student Number: {student.student_n}, Subject: {n_to_subject[student.subject_n]} Number {student.subject_n}, Exam Number: {student.exam_n}")
+        print(f"Student Number: {student.student_n}, Subject: {n_to_subject[student.subject_n]} Number {student.subject_n}, Exam Number: {student.exam_n}")
 
-    # At this point, we have an ordered set of students from the exams with the shortest to longest time in a list
+    # At this point, we have an ordered set of students from the exams with the shortest to the longest time in a list
+    # Assign seats based on the following algorithm
--- README.md
+++ README.md
@@ -6,9 +6,31 @@
 ## Tutorial on testing intu.py
 Make sure you have installed python 3 on your system
 
+### Running the Code
 Run the following in the terminal of current working directory to run a test case
 
 ```commandline
-python3 intu.py < Input/TestX.txt > Output/TestX_output.txt
+python3 intu.py < Input/TestX.in > Output/TestX.out
 ```
+
+For example,
+
+```commandline
+python3 intu.py < Input/Test1.in > Output/Test1.out
+```
+ 
+- Input is stored as a text file ending with in
+- Output is stored as a text file ending with out
+
+### Creating your own tests
+
+Copy `Test1.in`
+
+1. Modify the name of `TestX.in` 
+2. The first line represents the number of rows in each column
+   - An input of "1 1" means there are two columns with 1 row each
+3. The rest of lines contain information on the student in the following format
+   - {Student Number} {Subject} {Exam Number}
+4. Make sure the bottom of the file has two empty lines
+   - This is just due to implementation :p 
 
