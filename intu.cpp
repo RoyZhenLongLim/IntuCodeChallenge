c454087553aad4db94b5357aade42b88d127cb48
Sai Nair <lifeofsi3.1415227@gmail.com>
2022-06-17 20:25:49 +1000

    Fixed for penultimate version

--- intu.cpp
+++ intu.cpp
@@ -5,81 +5,123 @@
 #include <vector>
 #include <queue>
 #include <map>
+#include <utility>
+#include <tuple>
+#include <cstring>
 using namespace std;
+
+typedef tuple<string, int, int> triple;
 
 class person {
 	public:
 		string name;
 		string subject;
 		int exam_num;
+		int exam_length;
 };
 
-
-const string delimit = " ";
 // Exams sorted in terms of length of exam
-map<string, int> exam_type = {
-	{"HMX2", 0},
-	{"HM", 1},
-	{"HC", 2},
-	{"HP", 3},
-	/*{"PMX", 4},
-	{"PM", 5},
-	{"PC", 6},
-	{"PP", 7},
-	{"Y10",8},*/
-	{"HMX1", 10}
-};
+map<triple, int> exam_type = {};
 
-// Custom operator to check if the lhs exam is 'less' than the rhs exam
-// Sort by subject (in decreasing number) then exam number then individual name when student is added to the queue
+map<triple, int> exam_copy = {};
+
+// New operator return true if lhs > rhs i.e. lhs needs to be seated before rhs
 class comp{
 	public:
 		bool operator()(person lhs, person rhs) {
-			if (exam_type[lhs.subject.c_str()] != exam_type[rhs.subject.c_str()]) return exam_type[lhs.subject.c_str()] < exam_type[rhs.subject.c_str()];
-			else {
-				if (lhs.subject.c_str() != rhs.subject.c_str()) return lhs.subject.c_str() < rhs.subject.c_str();
-				else {
-					if (lhs.exam_num != rhs.exam_num) return lhs.exam_num < rhs.exam_num;
-					else return lhs.name.c_str() < rhs.name.c_str();
-				}
-			}
+			if (lhs.exam_length != rhs.exam_length) return lhs.exam_length > rhs.exam_length;
+			else if (exam_copy[make_tuple(lhs.subject.c_str(), lhs.exam_num, lhs.exam_length)] != exam_copy[make_tuple(rhs.subject.c_str(), rhs.exam_num, rhs.exam_length)]) return exam_copy[make_tuple(lhs.subject.c_str(), lhs.exam_num, lhs.exam_length)] < exam_copy[make_tuple(rhs.subject.c_str(), rhs.exam_num, rhs.exam_length)];
+			else if (strcmp(lhs.subject.c_str(), rhs.subject.c_str()) != 0) return strcmp(lhs.subject.c_str(), rhs.subject.c_str()) > 0;
+			else if (lhs.exam_num != rhs.exam_num) return lhs.exam_num > rhs.exam_num;
+			else return strcmp(lhs.name.c_str(), rhs.name.c_str()) > 0;
 		}
 };
 
 
 int main(){
 	priority_queue<person, vector<person>, comp> students;
-	int cols, stu_num, max_row = -1;
+	int cols, stu_num, max_row = -1, max_len = -1;
+	char door;
 	
 	// Scanning in layout of room
-	scanf("%d", &cols);
+	scanf("%d %c", &cols, &door);
 	int rows[cols];
+	int remaining[cols];
 	for (int i = 0; i<cols; i++) {
 		scanf("%d", &rows[i]);
+		remaining[i] = rows[i];
 		if (rows[i] > max_row) max_row = rows[i];
 	}
 	
 	// Scanning in student info
 	scanf("%d", &stu_num);
-	for(int i = 0; i<stu_num; i++) {
+	person stu[stu_num];
+	
+	for(int i = 0; i < stu_num; i++) {
 		person student;
 		scanf("%s", student.name.c_str());
 		scanf("%s", student.subject.c_str());
-		scanf("%d", student.exam_num);
-		students.push(student);
-	}
-	
-	// Seating students with shorter exams closer to door
-	person seating[max_row][cols];
-	for(int i = 0; i<max_row; i++) {
-		for(int j = cols-1; j>=0; j--) { //i and j value can be altered if corner of door changes
-			if (!students.empty() && rows[j] > i) {
-				seating[i][j] = students.top();
-				students.pop();				
-			}
+		scanf("%d", &student.exam_num);
+		scanf("%d", &student.exam_length);
+		exam_type[make_tuple(student.subject.c_str(), student.exam_num, student.exam_length)]++;
+		exam_copy[make_tuple(student.subject.c_str(), student.exam_num, student.exam_length)]++;
+		stu[i] = student;
+		if (student.exam_length > max_len) {
+			max_len = student.exam_length;
 		}
 	}
 	
+	for (int i = 0 ; i < stu_num; i++) {
+		students.push(stu[i]);
+	}
+
+	// Arranging students
+	person seating[max_row][cols];
+	while (!students.empty()) {
+		triple subj = make_tuple(students.top().subject.c_str(), students.top().exam_num,students.top().exam_length);
+		if (students.top().exam_length == max_len) {
+			int max_col = -1;
+			int num = -1;
+			for (int j = cols; j-->0;) {
+				if (remaining[j] > num) {
+					max_col = j;
+					num = remaining[j];
+				}
+			}
+			int k = 0;
+			for (int i = rows[max_col] - remaining[max_col]; i < rows[max_col] && i - (rows[max_col] - remaining[max_col]) < exam_type[subj]; i++) {
+				seating[i][max_col] = students.top();
+				students.pop();
+				k++;
+			}
+			exam_type[subj] -= k;
+			remaining[max_col] -= k;
+		} else if (door == 'l' || door == 'L') {
+			for (int i = 0; i < cols && !students.empty() && exam_type[subj] > 0; i++) {
+				for (int j = 0; j < max_row && !students.empty() && exam_type[subj] > 0; j++) {
+					if (rows[i] > j) {
+						seating[j][i] = students.top();
+						students.pop();
+						exam_type[subj]--;
+						remaining[i]--;
+					}
+				}
+			}
+		} else {
+			for (int i = cols; i--> 0 && !students.empty() && exam_type[subj] > 0;) {
+				for (int j = 0; j < max_row && !students.empty() && exam_type[subj] > 0; j++) {
+					if (rows[i] > j) {
+						seating[j][i] = students.top();
+						students.pop();
+						exam_type[subj]--;
+						remaining[i]--;
+					}
+				}
+			}
+		}
+	}
+
+
 	// Printing seating arrangement
 	// Seats students row by row, starting from the front
 	for(int i = 0; i<max_row; i++) {
@@ -90,10 +132,5 @@ int main(){
 		}
 		printf("\n");
 	}
-	
-	// Debugging for printing student details
-	//for(;!students.empty();students.pop()){
-	//	printf("%s %s %d\n", students.top().name.c_str(), students.top().subject.c_str(), students.top().exam_num);
-	//}
 	return 0;
 }
